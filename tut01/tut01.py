2302e28c529fbf5f3891989eae7919e3e1fa2105
ABHAY CHAHAR <ronniech1602@gmail.com>
2022-09-14 20:02:18 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,12 +1,33 @@
 import os
+import csv
 os.system("cls")
 
-def factorial(x):
-    if x==1 or x==0:
-     return 1
-    else:
-     return (x*factorial(x-1))   
+#CREATING 3 EMPTY LISTS TO STORE VALUES OF U',V',W'
+LIST1=[]
+LIST2=[]
+LIST3=[]
 
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
-print(factorial(x))
+#OPENING THE OCTANT_INPUT.CSV FILE IN READ MODE 
+with open('octant_input.csv','r') as file:
+    reader = csv.reader(file)
+    
+    #DEFINING SUMMATION VARIABLES WITH INITIAL VALUES OF 0 
+    U_SUMMATION=0
+    V_SUMMATION=0
+    W_SUMMATION=0
+
+    #CALCULATING SUMMATION OF U,V AND W VALUES 
+    for row in reader:
+        if (row[1]!='U'): #TO SKIP THE HEADER ROW
+            U_SUMMATION+=float(row[1]) #ADDING THE FLOAT VALUE OF ROW ELEMENTS AT INDEX POSITION 1 UNTIL THE ROWS END
+        if (row[2]!='V'): 
+            V_SUMMATION+=float(row[2]) 
+        if (row[3]!='W'): 
+            W_SUMMATION+=float(row[3]) 
+
+n=29745 #NUMBER OF ROWS EXCEPT HEADER
+
+#CALCULATION OF AVERAGES
+u_average=U_SUMMATION/n
+v_average=V_SUMMATION/n
+w_average=W_SUMMATION/n

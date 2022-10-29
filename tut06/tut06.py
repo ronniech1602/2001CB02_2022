b78c799429c802e02015f8a0e967516246f3178a
ABHAY CHAHAR <ronniech1602@gmail.com>
2022-10-30 00:26:23 +0530

    Final program

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -2,20 +2,24 @@ from datetime import datetime
 start_time = datetime.now()
 import pandas as pd
 
+#creating databases using pandas for both input csv files
 dt=pd.read_csv("input_attendance.csv")
 dtx=pd.read_csv("input_registered_students.csv")
 dt=dt.dropna()
-dt["Timestamp"]=pd.to_datetime(dt["Timestamp"],dayfirst=1)
+dt["Timestamp"]=pd.to_datetime(dt["Timestamp"],dayfirst=1) #coverting strings to datetime format
 
+#coverting column wise database to lists 
 timestamp=list(dt["Timestamp"])
 attendance=list(dt["Attendance"])
 roll_no=list(dtx["Roll No"])
 name=list(dtx["Name"])
 
+#creating a time interval for comparison
 comp_time=["14:00","15:00"]
 for i in range(0,len(comp_time)):
     comp_time[i]=datetime.strptime(comp_time[i],'%H:%M')
 
+#creating two lists containing the roll no. and names seperately , and i will work with roll no.
 attendance_roll=[]
 attendance_name=[]
 for i in attendance:
@@ -23,6 +27,7 @@ for i in attendance:
     attendance_roll.append(j[0])
     attendance_name.append(j[1])
     
+#dropping duplicates from the duplicates
 list_tmp=[]
 c=0
 for i in range(c,len(timestamp)):
@@ -36,7 +41,8 @@ for i in range(c,len(timestamp)):
             c=j+1            
                 
 dt=dt.drop(index=list_tmp)
-    
+  
+#calculating actual and fake attendance  
 Actual_attendance=[]
 Fake_attendance=[]
 for i in range(0,len(roll_no)):
@@ -53,7 +59,8 @@ for i in range(0,len(roll_no)):
                 b+=1
     Actual_attendance.append(a)
     Fake_attendance.append(b)
-    
+
+#calculating the total number of lectures taken    
 lec_date_data=[]
 for i in range(0,len(attendance_roll)):
     if timestamp[i].isoweekday()==1 or timestamp[i].isoweekday()==4:
@@ -62,15 +69,18 @@ for i in range(0,len(attendance_roll)):
 lec_taken=set(lec_date_data)
 Total_lec_taken=len(lec_taken)
 
+#calculating the count of absent (total lectures taken = actual attendance + absent count)
 Absent_count=[]
 for i in range(0,len(roll_no)):
     c=0
     c=Total_lec_taken-Actual_attendance[i]
     Absent_count.append(c)
-    
+
+#creating a folder 'output' to hold all the output files    
 import os
 os.mkdir('Output')
-    
+
+#writing to individual csv files    
 import csv
 for i in range(0,len(roll_no)):
     with open('Output/{}.csv'.format(str(roll_no[i])),'w',newline='') as f:
@@ -78,12 +88,14 @@ for i in range(0,len(roll_no)):
         writer.writerow(['Roll_No','Name','Total_lecture_taken','Attendance_count_actual','Attendance_count_fake','Attendance_count_absent','Percentage'])
         writer.writerow([roll_no[i],name[i],Total_lec_taken,Actual_attendance[i],Fake_attendance[i],Absent_count[i],'{0:.2f}'.format(Actual_attendance[i]*100/Total_lec_taken)])
 
+#con contains the rows to be written on the consolidated csv file
 con=[]
 for i in range(0,len(roll_no)):
     l=[]
     l=[roll_no[i],name[i],Total_lec_taken,Actual_attendance[i],Fake_attendance[i],'{0:.2f}'.format(Actual_attendance[i]*100/Total_lec_taken)]
     con.append(l)
     
+#writing to consolidated file
 with open('Output/Attendance_report_consolidated.csv','w',newline='') as g:
     writer=csv.writer(g)
     writer.writerow(['Roll_No','Name','Total_lecture_taken','Attendance_count_actual','Attendance_count_fake','Attendance_count_absent','Percentage'])
@@ -93,3 +105,5 @@ with open('Output/Attendance_report_consolidated.csv','w',newline='') as g:
 #This shall be the last lines of the code.
 end_time = datetime.now()
 print('Duration of Program Execution: {}'.format(end_time - start_time))
+
+#program completed to my best capacity ( made on SPYDER IDE 5.3.3 with PYTHON 3.8.10 64 BIT)

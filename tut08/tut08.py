216b05ce39294a3a72e4c535467bceec57bc14de
ABHAY CHAHAR <ronniech1602@gmail.com>
2022-11-09 22:18:19 +0530

    indian bowling card generated

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -34,9 +34,9 @@ for i in tmp:
     ind.append(b[1])
     
 pak_s=set(pak)
-pak_s=list(pak)
+pak_s=list(pak_s)
 ind_s=set(ind)
-ind_s=list(ind)
+ind_s=list(ind_s)
 
 runf=[]
 runs=[]
@@ -105,6 +105,62 @@ for i in pak_team.keys():
     d=[c_0,c_1,c_2,c_3,c_4,c_5,c_6,c_wic,c_wide,c_bye,c_lb,c_nb]
     c.append(d)
 
+cc=[]
+for i in ind_team.keys():
+    c_0=0
+    c_1=0
+    c_2=0
+    c_3=0
+    c_4=0
+    c_5=0
+    c_6=0
+    c_wic=0
+    c_wide=0
+    c_bye=0
+    c_lb=0
+    c_nb=0
+    for j in range(0,len(overs)):
+        if ind[j]==ind_team[i]:
+            if runf[j]=="no":
+                if runs[j]=="ball":
+                    c_nb+=1
+                if runs[j]=="run":
+                    c_0+=1
+            if runf[j]=="1":
+                if runs[j]=="run":
+                    c_1+=1
+            if runf[j]=="2":
+                if runs[j]=="runs":
+                    c_2+=1
+            if runf[j]=="3":
+                if runs[j]=="runs":
+                    c_3+=1
+            if runf[j]=="5":
+                if runs[j]=="runs":
+                    c_5+=1
+            if runf[j]=="FOUR":
+                c_4+=1
+            if runf[j]=="SIX":
+                c_6+=1
+            if runf[j]=="out":
+                c_wic+=1
+            if runf[j]=="wide":
+                c_wide+=1
+            if runs[j]=="wides":
+                c_wide+=int(runf[j])
+            if runs[j]=="byes":
+                try:
+                    c_bye+=int(runf[j])
+                except:
+                    pass
+            if runs[j]=="legbyes" or runs[j]=="legbye":
+                try:
+                    c_lb+=int(runf[j])
+                except:
+                    pass
+    d=[c_0,c_1,c_2,c_3,c_4,c_5,c_6,c_wic,c_wide,c_bye,c_lb,c_nb]
+    cc.append(d)
+    
 #This shall be the last lines of the code.
 end_time = datetime.now()
 print('Duration of Program Execution: {}'.format(end_time - start_time))
